28256e97a7e6359af8fccd20e3f8a8db8f59991f
Trương Nguyễn Quang Thái <87488896+TNQT14@users.noreply.github.com>
2024-01-04 23:40:06 +0700

    using router

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
+import userRouter from './router/users.js'
 dotenv.config()
-const app = express
+const app = express()
 
-port = process.env.PORT;
-app.listen(port ?? 3000, async (req, res) =>
+const port = process.env.PORT ?? 3000
+
+app.get('/', (req, res) => {
+    res.send("Response from root router, ahihi")
+})
+
+app.use('/users', userRouter)
+
+app.listen(port, async (req, res) =>
     console.log(`Listen on port: ${port}`)
 )
